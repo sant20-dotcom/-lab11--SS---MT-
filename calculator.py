05bd3485bf010f1bbbf0c958b2b0474512b1d23b
sant20-dotcom <stevensant20@gmail.com>
2025-11-12 13:32:50 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -1,11 +1,4 @@
 import math
-def square_root(a):
-    if a < 0:
-        raise ValueError
-    return math.sqrt(a)
-
-def hypotenuse(a, b):
-    return hypotenuse(a, b)
 
 def add(a, b):
     return a + b
@@ -31,3 +24,10 @@ def log(a, b):
 def exp(a, b):
     return a ** b
 
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return hypotenuse(a, b)
