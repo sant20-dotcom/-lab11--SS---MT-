c9ffec963dba98e7da39d7a013d8f5955c1829c8
Muhammad Taha <muhammadtaha173456@gmail.com>
2025-11-12 12:44:12 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -18,9 +18,15 @@ def mul(a, b):
     return a*b
 
 def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError
     return a / b
 
 def log(a, b):
+    if a<=0 or a ==1:
+        raise ValueError
+    if b <= 0:
+        raise ValueError
     return math.log(a, b)
 
 def exp(a, b):
