b0d9d093bbb71c99dafec3e2ecddbc78b5c8421e
sant20-dotcom <stevensant20@gmail.com>
2025-11-12 13:09:49 -0500

    modified calculator

--- calculator.py
+++ calculator.py
@@ -1,4 +1,11 @@
 import math
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return hypotenuse(a, b)
 
 def add(a, b):
     return a + b
