ed7e1e414eb2b63bfa61ae0ce3d2070518ea9df1
sant20-dotcom <stevensant20@gmail.com>
2025-11-12 13:30:41 -0500

    merged changes

--- test_calculator.py
+++ test_calculator.py
@@ -2,34 +2,13 @@ import unittest
 from calculator import *
 
 class TestCalculator(unittest.TestCase):
-    ######### Partner 2
-    # def test_add(self): # 3 assertions
-    #     fill in code
 
-    # def test_subtract(self): # 3 assertions
-    #     fill in code
-    # ##########################
 
     ######## Partner 1
     # def test_multiply(self): # 3 assertions
     #     fill in code
 
     # def test_divide(self): # 3 assertions
-    #     fill in code
-    # ##########################
-
-    ######## Partner 2
-    # def test_divide_by_zero(self): # 1 assertion
-    #     # call division function inside, example:
-    #     # with self.assertRaises(<INSERT_ERROR_TYPE>):
-    #     #     div(0, 5)
-    #     fill in code
-
-    # def test_logarithm(self): # 3 assertions
-    #     fill in code
-
-    # def test_log_invalid_base(self): # 1 assertion
-    #     # use same technique from test_divide_by_zero
     #     fill in code
     # ##########################
     
