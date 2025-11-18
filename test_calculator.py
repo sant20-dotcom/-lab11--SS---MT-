490bbea3e43a183a901edaec33ba13509a1b53d9
sant20-dotcom <stevensant20@gmail.com>
2025-11-18 17:56:44 -0500

    modified TestCalculator

--- test_calculator.py
+++ test_calculator.py
@@ -3,6 +3,34 @@ from calculator import *
 
 class TestCalculator(unittest.TestCase):
 
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertAlmostEqual(add(1.2, 3.4), 4.6)
+
+    def test_sub(self):
+        self.assertEqual(sub(5, 3), 2)
+        self.assertEqual(sub(0, 5), -5)
+        self.assertAlmostEqual(sub(5.5, 2.2), 3.3)
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            div(10, 0)
+
+    def test_logarithm(self):
+        self.assertAlmostEqual(log(100, 10), 2)
+        self.assertAlmostEqual(log(math.e, math.e), 1)
+
+    def test_log_invalid_base(self):
+        with self.assertRaises(ValueError):
+            log(100, 1)
+
+        with self.assertRaises(ValueError):
+            log(100, -5)
+
+        with self.assertRaises(ValueError):
+            log(-100, 10)
+
     def test_multiply(self):
         self.assertEqual(mul(2, 3), 6)
         self.assertEqual(mul(-2, 3), -6)
